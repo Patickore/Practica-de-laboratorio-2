20fb1a12c815f1c6e480d8e2140d23d945bec1a4
Patrick Orellana <Computosala42022@ugb.edu.sv>
2022-08-12 11:08:51 -0600

    Actualizacion

--- Practica de laboratorio 2/Program.cs
+++ Practica de laboratorio 2/Program.cs
@@ -6,7 +6,28 @@ namespace Practica_de_laboratorio_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese su nota promedio:");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota >= 0 && nota <= 10)
+            {
+                if (nota >= 6)
+                {
+                    Console.WriteLine("Aprobado");
+                }
+                else if (nota >= 5)
+                {
+                    Console.WriteLine("Reposicion");
+                }
+                else
+                {
+                    Console.WriteLine("Reprobado");
+                }
+            }
+            else
+            {
+                Cosole.Writeline("La nota no es correcta");
+            }
         }
     }
 }
