7aef21aa638f689b13ed94ec1d223904a6a25929
Patrick Orellana <Computosala42022@ugb.edu.sv>
2022-08-12 12:00:39 -0600

    Actualizacion

---
+++ App5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace App5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.WriteLine("Ingrese un numero entero (Con 0 se finaliza):");
+                num = int.Parse(Console.ReadLine());
+
+                if (num >= 100)
+                {
+                    Console.WriteLine("Tiene 3 digitos.");
+                }
+                else if (num >= 10)
+                {
+                    Console.WriteLine("Tiene 2 digitos.");
+                }
+                else
+                {
+                    Console.WriteLine("Tiene 1 digito.");
+                }
+            } while (num != 0);
+        }
+    }
+}
