ac32e0313b272d8056bc34d4fce264d3c0fde265
Patrick Orellana <Computosala42022@ugb.edu.sv>
2022-08-12 11:36:10 -0600

    Actualizacion

---
+++ App3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero a procesar:");
+            int multiplo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el limite:");
+            int limite = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= limite; i++)
+            {
+                if (i %multiplo == 0)
+                {
+                    Console.Write(i + "");
+                }
+            }
+        }
+    }
+}
